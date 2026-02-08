576094dc4276ca41d5abaf9f7fc1a90116666d57
Russ Gunther <rgunther30@gmail.com>
2026-02-08 12:10:47 -0500

    Use macros for defaults to simplify from explicit definitions

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -7,20 +7,8 @@ defmodule Tunez.Music.Artist do
   end
 
   actions do
-    create :create do
-      accept [:name, :biography]
-    end
-
-    read :read do
-      primary? true
-    end
-
-    update :update do
-      accept [:name, :biography]
-    end
-
-    destroy :destroy do
-    end
+    defaults [:create, :read, :update, :destroy]
+    default_accept [:name, :biography]
   end
 
   attributes do
