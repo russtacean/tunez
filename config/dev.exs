6155e886c85ae2b9d7c9b9f225bd2e4e00d4954d
Russ Gunther <rgunther30@gmail.com>
2026-02-14 11:33:57 -0500

    Chap5: Add auth debug logging for local dev only, leave out of prod for security reasons

--- config/dev.exs
+++ config/dev.exs
@@ -85,3 +85,5 @@ config :phoenix_live_view,
 
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
+
+config :ash_authentication_phoenix, :debug, true
